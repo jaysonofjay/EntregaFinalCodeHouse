851a2d424c6b0e5914acaabbf1f5ebf7d865b871
jaysonofjay <juanjose.perezgarcia85@gmail.com>
2022-09-10 20:14:44 -0700

    SegundaEntregaFinal
    Segunda entrega final en respuesta comentario de FACU el dia 09/10

--- Controllers/VentaController.cs
+++ Controllers/VentaController.cs
@@ -18,49 +18,103 @@ namespace EntregaFinal.Controllers
         }
 
 
+        // NOTA: ***Funcion CrearVenta modificada a base de comentario de Facu el 9/10/2022  ver la parte inferior para respuesta. ** 
+
+        //[HttpPost]
+        //public bool CrearVenta([FromBody] PostProductoVendido productovendido)
+        //{
+        //    /* Cargar Venta: Recibe una lista de productos y el número de IdUsuario de quien la efectuó, 
+        //        problema : se me complico esta parte de ingresar varios productos en una lista para cargar la venta 
+        //        resolucion:  el Post de venta solo carga un articulo, pero registra venta nueva en ventas y productos vendidos asi como descontar el stock en productos.*/
+        //    int nuevoIDVenta = 0;
+        //    bool productoAgregado = false;
+        //    bool stockModificado = false;
+        //    try
+        //    {
+        //        // primero cargar una nueva Venta en la base de datos, 
+        //        nuevoIDVenta = VentaHandler.CrearVenta();
+        //        //  cargar los productos recibidos en la base de ProductosVendidos
+        //        productoAgregado = ProductosVendidosHandler.AgregarProductoVendido(new ProductoVendido
+        //        {
+        //            Stock = productovendido.Stock,
+        //            IdProducto = productovendido.IdProducto,
+        //            IdVenta = nuevoIDVenta
+        //        });
+        //        //  descontar el stock en la base de productos por el otro
+        //        stockModificado = ProductoHandler.ModificarStockdeProducto(new Producto 
+        //        {
+        //            Stock = productovendido.Stock, 
+        //            Id = productovendido.IdProducto 
+        //        }  , nuevoIDVenta);
+        //        if (productoAgregado && stockModificado) { return true; } else { return false; }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine(ex.Message);
+        //        return false;
+        //    }
+        //}
+
+
+        // NOTA  >  CAMBIO REALIZADO 9/10/2022  RESPUESTA DE COMENTARIO DE FACU. 
+        /* (Punto de desafio Cargar Venta): Recibe una lista de productos y el número de IdUsuario de quien la efectuó, 
+            problema : se me complico esta parte de ingresar varios productos en una lista para cargar la venta 
+            resolucion:  Aggregar List<Class>  para poder recibir una collection de productos vendidos e iterar por cada uno de ellos para poder subtraer la cantidad*/
+
+
         [HttpPost]
-        public bool CrearVenta([FromBody] PostProductoVendido productovendido)
+        public bool CrearVenta2([FromBody] List<PostProductoVendido> productovendidos)
         {
-
-
-            /* Cargar Venta: Recibe una lista de productos y el número de IdUsuario de quien la efectuó, 
-                problema : se me complico esta parte de ingresar varios productos en una lista para cargar la venta 
-                resolucion:  el Post de venta solo carga un articulo, pero registra venta nueva en ventas y productos vendidos asi como descontar el stock en productos.*/
-
             int nuevoIDVenta = 0;
-            bool productoAgregado = false;
-            bool stockModificado = false;
+      
+            //primero cargar una nueva Venta en la base de datos,
+            nuevoIDVenta = VentaHandler.CrearVenta();
 
             try
             {
-                // primero cargar una nueva Venta en la base de datos, 
-                nuevoIDVenta = VentaHandler.CrearVenta();
-
-                //  cargar los productos recibidos en la base de ProductosVendidos
-                productoAgregado = ProductosVendidosHandler.AgregarProductoVendido(new ProductoVendido
+                int conteoAgregados = 0;
+                foreach (PostProductoVendido productovendido in productovendidos)
                 {
-                    Stock = productovendido.Stock,
-                    IdProducto = productovendido.IdProducto,
-                    IdVenta = nuevoIDVenta
+                   
+                    bool productoAgregado = false;
+                    bool stockModificado = false;
 
-                });
+                    //  cargar los productos recibidos en la base de ProductosVendidos
+                    productoAgregado = ProductosVendidosHandler.AgregarProductoVendido(new ProductoVendido
+                    {
+                        Stock = productovendido.Stock,
+                        IdProducto = productovendido.IdProducto,
+                        IdVenta = nuevoIDVenta
 
-                //  descontar el stock en la base de productos por el otro
-                stockModificado = ProductoHandler.ModificarStockdeProducto(new Producto 
-                {
-                    Stock = productovendido.Stock, 
-                    Id = productovendido.IdProducto 
-                }  , nuevoIDVenta);
+                    });
+
+                    //  descontar el stock en la base de productos por el otro
+                    stockModificado = ProductoHandler.ModificarStockdeProducto(new Producto
+                    {
+                        Stock = productovendido.Stock,
+                        Id = productovendido.IdProducto
+                    });
+
+                    // contar cada producto que es agregado y descontado del stock
+                    if (productoAgregado && stockModificado) { conteoAgregados++;} 
+                }
+
+                /* si no todos los productos agregados son igual a la cantidad de productions en la coleccion ingresada, entonces se genero un error dentro del foreach 
+                        por ejemplo un id de producto que no existe */
+                if (conteoAgregados == productovendidos.Count) { return true; } else { return false; }
 
 
-                if (productoAgregado && stockModificado) { return true; } else { return false; }
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.Message);
-                return false;
+                    Console.WriteLine(ex.Message);
+                    return false;
+            
             }
+
+
         }
+
 
 
         // Eliminar venta
